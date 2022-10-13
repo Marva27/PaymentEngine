09345546e69b5f1ad45f0dbe1c1546e9df004b99
Srinivasan Ramu <srinimarva@gmail.com>
2022-10-12 22:27:30 -0500

    Add files via upload

---
+++ src/main/java/com/paymentengine/www/PaymentEngineApplication.java
@@ -0,0 +1,13 @@
+package com.paymentengine.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymentengine/www/PaymentEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentengine.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
